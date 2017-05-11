0f33221096de49e79fa7f51414cd62d5b4b65a8b
ciwei <690962580@qq.com>
2017-05-11 13:20:54 +0800

    优化算法

--- 1031.c
+++ 1031.c
@@ -2,27 +2,23 @@
 int main()
 {
 	int n,i,j,sum,d[]={7,9,10,5,8,4,2,1,6,3,7,9,10,5,8,4,2};
-	char b[100][19],check[]={'1','0','X','9','8','7','6','5','4','3','2'};
+	char b[19],check[]={'1','0','X','9','8','7','6','5','4','3','2'};
 	scanf("%d",&n);
 	for(i=0;i<n;i++)
 	{
-		scanf("%s",b[i]);
+		scanf("%s",b);
 		sum=0;
 		for(j=0;j<17;j++)
 		{
-			if(b[i][j]!='0') sum+=(b[i][j]-'0')*d[j];
+			if(b[j]!='0') sum+=(b[j]-'0')*d[j];
 		}
 		sum%=11;
-		if(check[sum]==b[i][17]){
+		if(check[sum]==b[17]){
 			i--;
 			n--;
 		}
+        else puts(b);
 	}
 	if(n==0) printf("All passed\n");
-	else{
-		for(i=0;i<n;i++){
-			puts(b[i]);
-		}
-	}
 	return 0;
 }
