b868bcc39b39d585ef6e67413ac2fc92d0d0f274
ciwei <690962580@qq.com>
2017-07-17 12:33:58 +0800

    调整结构

--- 1069.c
+++ 1069.c
@@ -1,19 +1,21 @@
 #include<stdio.h>
 #include<string.h>
 /* 利用二分查找算法查重，然后用插入排序方
-便下次查找，利用指针的特性对字符串排序; */
+便下次查找，利用指针的特性对字符串移动; */
 int main()
 {
-    int n,m,l,i=1,j,k,low,high,mid;
+    int n,m,l,i,j,k,low,high,mid;
     scanf("%d%d%d",&n,&l,&m);
     k=n/l+1;
     char name[k][21],*pn[k],*p;
-    for(j=0; j<k; j++) //字符串与指针对应
+	//字符串与指针对应
+    for(i=0; i<k; i++)
     {
-        pn[j]=name[j];
+        pn[i]=name[i];
     }
+	i=1;
     k=0;
-    //过滤第m名之前的字符串
+	//过滤第m名之前的字符串
     while(i!=m)
     {
         i++;
@@ -24,35 +26,36 @@ int main()
             return 0;
         }
     }
-    //输出并记录第一个中奖的名字
+	//输出并记录第一个中奖的名字
     scanf("%s",name[k++]);
     puts(name[0]);
     i++;
+    m+=l;
     while(i<=n)
     {
-        //过滤字符串
-        j=1;
-        while(j!=l)
+		//过滤第m名之前的字符串
+        while(i!=m)
         {
             i++;
-            j++;
             scanf("%*s");
-            if(i>n) return 0;
+            if(i>n)
+            {
+                return 0;
+            }
         }
-loop:
         scanf("%s",name[k]);
-        //二分查重
+		//二分查重
         low=0;
         high=k-1;
         while(low<=high)
         {
             mid=(low&high)+((low^high)>>1);
-            m=strcmp(pn[mid],pn[k]);
-            if(m>0)
+            j=strcmp(pn[mid],pn[k]);
+            if(j>0)
             {
                 high=mid-1;
             }
-            else if(m<0)
+            else if(j<0)
             {
                 low=mid+1;
             }
@@ -61,16 +64,14 @@ loop:
                 break;
             }
         }
-        //如果相等则输入下一位继续查重，否则记录输出
-        if(m==0)
+		//如果相等则输入下一位继续查重，否则记录输出
+        if(j==0)
         {
-            if(++i>n) break;
-            goto loop;
+            m++;
         }
         else
         {
             puts(pn[k]);
-            //插入排序
             while(low<k)
             {
                 p=pn[k];
@@ -78,6 +79,7 @@ loop:
                 pn[low++]=p;
             }
             k++;
+			m+=l;
         }
         i++;
     }
